9089d3e27ed6216c2b840eb5844866d866839030
Cintia Silva <cintiasilva7300@gmail.com>
2022-10-11 20:27:46 -0300

    feat: ensure SigUpController return an error if no email is provaded and name

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('Signup Controller', () => {
        expect (httpResponse.statusCode).toBe(400)
        expect (httpResponse.body).toEqual(new Error('Missing param: name'))
     })
+
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequesr = {
+            body: {
+                name: 'cintia',
+                // email: 'cintia@gmail.com',
+                password:'123',
+                passwordConfirmation: '123',
+            }
+        }
+       const httpResponse =  sut.handle(httpRequesr)
+       expect (httpResponse.statusCode).toBe(400)
+       expect (httpResponse.body).toEqual(new Error('Missing param: email'))
+    })
 })
