7a952828e15e2b3cbbe6ff3f23025d12abfce647
Cintia Silva <cintiasilva7300@gmail.com>
2022-10-16 11:42:53 -0300

    refactor: group all protocols in a sigle file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,6 +1,9 @@
-import { Encrypter } from "./../../protocols/encrypter";
-import { AccountModel } from "./../../../domain/models/account";
-import { AddAccount, addAccountModel } from "src/domain/usecases/add-account";
+import {
+  AddAccount,
+  addAccountModel,
+  AccountModel,
+  Encrypter,
+} from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
