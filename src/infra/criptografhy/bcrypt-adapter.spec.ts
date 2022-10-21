399900dd0dd22f5640837a1254d36a3e3ffc924d
Cintia Silva <cintiasilva7300@gmail.com>
2022-10-21 20:02:38 -0300

    feat: ensure BcryptAdapter returns a hash on sucess

--- src/infra/criptografhy/bcrypt-adapter.spec.ts
+++ src/infra/criptografhy/bcrypt-adapter.spec.ts
@@ -1,5 +1,12 @@
 import * as bcrypt from "bcrypt";
+import { resolve } from "path";
 import { BcriptAdapter } from "./bcript-adapter";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
 
 describe("Bcrypt Adapter", () => {
   test("Shoult call bcrypt with correct value", async () => {
@@ -8,5 +15,12 @@ describe("Bcrypt Adapter", () => {
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("Shoult return a hash on sucess", async () => {
+    const salt = 12;
+    const sut = new BcriptAdapter(salt);
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash");
   });
 });
