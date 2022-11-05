418696a3b86e76993e1056cec9c61cf04a130843
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-05 13:10:26 -0300

    feat: ensure logControllerDecorator returns the some result of the controller

--- src/main/decorators/log.spac.ts
+++ src/main/decorators/log.spac.ts
@@ -51,4 +51,23 @@ describe("LogController Decorator", () => {
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test("should return the same result of the controller", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: "any_mail@mail.com",
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const HttpResponse = await sut.handle(httpRequest);
+    expect(httpRequest).toEqual({
+      statusCode: 200,
+      body: {
+        name: "Cintia",
+      },
+    });
+  });
 });
