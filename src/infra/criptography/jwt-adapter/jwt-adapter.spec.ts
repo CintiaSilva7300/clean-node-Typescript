d11a411a144762a74b67af423ffce60eefbd12f9
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-19 12:57:58 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,5 +1,6 @@
 import { JwtAdapter } from "./jwt-adapter";
 import jwt from "jsonwebtoken";
+import { rejects } from "assert";
 
 jest.mock("jsonwebtoken", () => ({
   async sign(): Promise<string> {
@@ -19,5 +20,14 @@ describe("jWT Adapter", () => {
     const sut = new JwtAdapter("secret");
     const acessToken = await sut.encrypt("any_id");
     expect(acessToken).toBe("any_token");
+  });
+
+  test("should throws if sign throws", async () => {
+    const sut = new JwtAdapter("secret");
+    jest.spyOn(jwt, "sign").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.encrypt("any_id");
+    await expect(promise).rejects.toThrow();
   });
 });
