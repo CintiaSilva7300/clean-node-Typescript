cd693018708c4c58e01f527f69bac4d11c368ed8
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-19 15:03:26 -0300

    refactor: remove duplicated code

--- src/presentation/helpers/validators/email-valitation.ts
+++ src/presentation/helpers/validators/email-valitation.ts
@@ -6,13 +6,10 @@ import { Validation } from "../../protocolos/validation";
 import { EmailValidator } from "../../protocolos/email-validator";
 
 export class EmailValidation implements Validation {
-  private readonly fieldName: string;
-  private readonly emailValidator: EmailValidator;
-
-  constructor(fieldName: string, emailValidator: EmailValidator) {
-    this.fieldName = fieldName;
-    this.emailValidator = emailValidator;
-  }
+  constructor(
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator
+  ) {}
   validate(input: any): Error | undefined {
     const isValid = this.emailValidator.isValid(input[this.fieldName]);
     if (!isValid) {
