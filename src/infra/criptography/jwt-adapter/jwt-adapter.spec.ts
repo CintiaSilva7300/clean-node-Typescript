f228f1b17b578549051fd4c5a2c7fe363ef1fbe1
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-19 12:49:53 -0300

    feat: ensure JwtAdapter returns a token sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,23 @@
 import { JwtAdapter } from "./jwt-adapter";
 import jwt from "jsonwebtoken";
 
+jest.mock("jsonwebtoken", () => ({
+  async sign(): Promise<string> {
+    return new Promise((resolve) => resolve("any_token"));
+  },
+}));
+
 describe("jWT Adapter", () => {
   test("should call sign with correct values", async () => {
     const sut = new JwtAdapter("secret");
     const signSpy = jest.spyOn(jwt, "sign");
     await sut.encrypt("any_id");
     expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
+  });
+
+  test("should return a token on sign success", async () => {
+    const sut = new JwtAdapter("secret");
+    const acessToken = await sut.encrypt("any_id");
+    expect(acessToken).toBe("any_token");
   });
 });
