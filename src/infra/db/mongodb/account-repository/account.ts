6e08913b3af9442a8214c769e489f99bd0445caf
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-19 13:33:59 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,13 @@
+import { AccountModel } from "./../../../../domain/models/account";
+import { LoadAccountByEmailRepository } from "./../../../../data/protocols/db/load-account-by-email-repository";
 import { addAccountModel } from "./../../../../domain/usecases/add-account";
 import { AddAccountRepository } from "src/data/protocols/db/add-account-repository";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class AccountMongoRepository implements AddAccountRepository {
-  async add(accountData: addAccountModel): Promise<any> {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
+  async add(accountData: addAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
     const result = await accountCollection.insertOne(accountData);
 
@@ -11,5 +15,11 @@ export class AccountMongoRepository implements AddAccountRepository {
       _id: result.insertedId,
     });
     return MongoHelper.map(account);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel | any> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email });
+    return account && MongoHelper.map(account);
   }
 }
