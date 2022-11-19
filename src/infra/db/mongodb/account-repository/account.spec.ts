66bbce6545c76527ae3724ee3de23469bc3b7752
Cintia Silva <cintiasilva7300@gmail.com>
2022-11-19 14:20:30 -0300

    feat: ensure AccountMongoRepository update the accoutn accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,23 @@ describe("Account Mongo Repository", () => {
     const account = await sut.loadByEmail("any_email@gmail.com");
     expect(account).toBeFalsy();
   });
+
+  test("should update the account accessToken on updateAccessToken success", async () => {
+    const sut = makeSut();
+    const res = await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@gmail.com",
+      password: "any_password",
+    });
+    const response: any = await accountCollection.findOne({
+      _id: res.insertedId,
+    });
+    expect(response.accessToken).toBeFalsy();
+    await sut.updateAccessToken(res.insertedId.toString(), "any_Token");
+    const account: any = await accountCollection.findOne({
+      _id: res.insertedId,
+    });
+    expect(account).toBeTruthy();
+    expect(account.accessToken).toBe("any_Token");
+  });
 });
